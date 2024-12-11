3d446e82fd45fa0f6dc400d76e5413c08fc73bca
moha254 <mohamedabukar412@gmail.com>
2024-12-11 13:38:04 +0300

    fixing background img

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -4,15 +4,16 @@ import { ChevronRight } from 'lucide-react';
 export function Hero() {
   return (
     <div className="relative h-screen">
-      <div 
-        className="absolute inset-0 bg-cover bg-center bg-no-repeat"
-        style={{
-          backgroundImage: 'url("https://images.unsplash.com/photo-1469854523086-cc02fe5d8800?ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80")',
-        }}
-      >
-        <div className="absolute inset-0 bg-black bg-opacity-50" />
-      </div>
-      
+    <div
+      className="absolute inset-0 bg-cover bg-center bg-no-repeat sm:bg-bottom md:bg-center"
+      style={{
+        backgroundImage:
+          'url("https://images.unsplash.com/photo-1469854523086-cc02fe5d8800?ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80")',
+      }}
+    >
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-black bg-opacity-50" />
+    </div>
       <div className="relative h-full flex items-center">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center md:text-left">
           <h1 className="text-4xl md:text-6xl font-bold text-white mb-6">
